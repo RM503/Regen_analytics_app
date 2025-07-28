2335e0b7ddfe34cf1bdce7efb28fb3ea52ab8e21
Rafid Mahbub <rafid.judas503@gmail.com>
2025-07-28 13:16:51 -0400

    upated landing page; login form added

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-# Frontend - landing page
+"""
+This is the main FastAPI backend that directs users to different
+parts of the dashboard
+"""
 
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.wsgi import WSGIMiddleware
 from fastapi.templating import Jinja2Templates
-from src.dash1.dash1_main import app as dash1
-from src.dash2.dash2_main import app as dash2
-from src.dash3.dash3_main import app as dash3
+from src.initial_market_data.dash0_main import app as dash0
+from src.polygon_generator.dash1_main import app as dash1
+from src.farmland_characteristics.dash2_main import app as dash2
+from src.farmland_statistics.dash3_main import app as dash3
 import logging
 from logging_config import setup_logging
 
@@ -17,13 +21,20 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates") # Landing page template
 
-# Mounts
+"""
+This is where the different dashboards are mounted
+"""
+
+# Frontend - landing page
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/logos", StaticFiles(directory="logos"), name="logos")
-app.mount("/dash1", WSGIMiddleware(dash1.server))
-app.mount("/dash2", WSGIMiddleware(dash2.server))
-app.mount("/dash3", WSGIMiddleware(dash3.server))
+
+# Dashboards start form here
+app.mount("/initial_market_data", WSGIMiddleware(dash0.server))
+app.mount("/polygon_generator", WSGIMiddleware(dash1.server))
+app.mount("/farmland_characteristics", WSGIMiddleware(dash2.server))
+app.mount("/farmland_statistics", WSGIMiddleware(dash3.server))
 
 @app.get("/")
-def root(request: Request):
+async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
