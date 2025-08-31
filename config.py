c20f16608a33c20ba5a8c73ef865226ebf2a339d
Rafid Mahbub <rafid.judas503@gmail.com>
2025-08-31 17:21:21 -0400

    config file for local testing

---
+++ config.py
@@ -0,0 +1,14 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+USE_LOCAL_DB = os.getenv("USE_LOCAL_DB", "false").lower() == "true"
+
+LOCAL_DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "database": "dash_local",
+    "user": "dash_user",
+    "password": "Thelookingglass*6"
+}
