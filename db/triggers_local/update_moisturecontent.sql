d09771ce58613c90ee774b14e6af367ecc48467c
Rafid Mahbub <rafid.judas503@gmail.com>
2025-09-16 19:31:41 -0400

    fix: moisturecontent trigger update

--- db/triggers_local/update_moisturecontent.sql
+++ db/triggers_local/update_moisturecontent.sql
@@ -1,30 +1,32 @@
 CREATE OR REPLACE FUNCTION update_moisturecontent()
 RETURNS TRIGGER AS $$
 BEGIN
-	DELETE FROM moisturecontent
-	WHERE region = NEW.region;
+    DELETE FROM moisturecontent
+    WHERE region = NEW.region;
 
-	INSERT INTO moisturecontent (region, moisture_content, counts)
-	SELECT
-		region,
-		moisture_content,
-		COUNT(*) AS counts
-	FROM (
-		SELECT
-			uuid,
-			region,
-			CASE
-				WHEN AVG(ndmi_max) OVER (PARTITION BY uuid, region) >= 0.38 THEN 'high'
-				WHEN AVG(ndmi_max) OVER (PARTITION BY uuid, region) >= 0.25
-					AND AVG(ndmi_max) OVER (PARTITION BY uuid, region) < 0.38 THEN 'medium'
-				WHEN AVG(ndmi_max) OVER (PARTITION BY uuid, region) >= 0.20
-					AND AVG(ndmi_max) OVER (PARTITION BY uuid, region) < 0.25 THEN 'approaching low'
-				ELSE 'low'
-			END AS moisture_content
-		FROM peakvidistribution
-		WHERE region = NEW.region
-	) categorized
-	GROUP BY region, moisture_content;
-	RETURN NEW;
+    INSERT INTO moisturecontent (region, moisture_content, counts)
+    SELECT
+        region,
+        moisture_content,
+        COUNT(DISTINCT uuid) AS counts
+        now() AS updated_at
+    FROM (
+        SELECT
+            uuid,
+            region,
+            CASE
+                WHEN AVG(ndmi_max) OVER (PARTITION BY uuid, region) >= 0.38 THEN 'high'
+                WHEN AVG(ndmi_max) OVER (PARTITION BY uuid, region) >= 0.25
+                     AND AVG(ndmi_max) OVER (PARTITION BY uuid, region) < 0.38 THEN 'medium'
+                WHEN AVG(ndmi_max) OVER (PARTITION BY uuid, region) >= 0.20
+                     AND AVG(ndmi_max) OVER (PARTITION BY uuid, region) < 0.25 THEN 'approaching low'
+                ELSE 'low'
+            END AS moisture_content
+        FROM peakvidistribution
+        WHERE region = NEW.region
+    ) categorized
+    GROUP BY region, moisture_content;
+
+    RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
