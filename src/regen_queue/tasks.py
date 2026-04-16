00ba7ae6081a9940e5a1c5947a591d1b28827ab4
Rafid Mahbub <92277868+RM503@users.noreply.github.com>
2026-04-15 21:12:32 -0400

    Initialize EE on task generation

--- src/regen_queue/tasks.py
+++ src/regen_queue/tasks.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
 from .celery_app import celery_app
-from utils.vi_timeseries import combined_timeseries
+from services.earth_engine_timeseries import combined_timeseries, initialize_ee
 
 @celery_app.task(bind=True, name="task.fetch_timeseries")
 def fetch_timeseries(self, df_roi_records: list[dict]) -> list[dict]:
-   """Fetch VI data for given ROI"""
-   df_roi = pd.DataFrame(df_roi_records)
-   df = combined_timeseries(df_roi)
+    """Fetch VI data for given ROI."""
+    initialize_ee()
 
-   return df.to_dict("records")
+    df_roi = pd.DataFrame(df_roi_records)
+    df = combined_timeseries(df_roi)
+
+    return df.to_dict("records")
